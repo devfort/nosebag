d52a2f2b54b832a7d432604f0afd1405e54e1cc8
James Coglan <jcoglan@gmail.com>
2019-04-29 17:03:22 +0100

    Begin filesystem storage backend
    This introduces a Store class that's based on Node's fs module, wrapped
    to return promises so we can use async/await. It so far implements:
    
    - list_recipes() returns a Promise<Array<String>> of the IDs of the
      recipes
    
    - is_recipe(id) returns a Promise<Boolean> to say whether a given ID
      exists and is a recipe, by detecting a directory containing a
      recipe.json file

---
+++ spec/storage/fs_spec.js
@@ -0,0 +1,20 @@
+const fs = require('fs'),
+      path = require('path'),
+      Store = require('../../lib/storage/fs');
+
+const STORE_PATH = path.resolve(__dirname, '..', '..', 'dr.syntax');
+
+describe('filesystem store', () => {
+  let store
+
+  beforeEach(() => {
+    store = new Store(STORE_PATH);
+  })
+
+  describe('list_recipes', () => {
+    it('returns a list of recipe IDs', async () => {
+      let recipes = await store.list_recipes();
+      expect(recipes).toEqual(['bacon-hash', 'chicken-chorizo-gumbo', 'rice-pudding']);
+    })
+  })
+})
