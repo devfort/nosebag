856572a564aa22c7942697f9144e827798ba0fc3
James Coglan <jcoglan@gmail.com>
2019-04-29 18:13:02 +0100

    Implement Store#get_recipe
    This function fetches a recipe from its JSON file, canonicalises its
    data, and returns a Promise containing a structure of the form:
    
        Recipe {
            name: String,
            attributes: [String],
            ingredients: [RecipeIngredient]
        }
    
        RecipeIngredient {
            ingredient: Ingredient,
            quantity: Quantity,
            prep: String
        }
    
        Ingredient {
            name: String
        }
    
        Quantity: {
            amount: Number,
            unit: String?
        }
    
    Ingredients do not yet have attributes as we're not consulting the
    ontology; we merely load the ingredients from the recipe and turn them
    into objects.

--- spec/storage/fs_spec.js
+++ spec/storage/fs_spec.js
@@ -2,7 +2,10 @@ const fs = require('fs'),
       path = require('path'),
       Store = require('../../lib/storage/fs');
 
+const { Recipe, RecipeIngredient, Quantity, Ingredient } = require('../../lib/models');
+
 const STORE_PATH = path.resolve(__dirname, '..', '..', 'dr.syntax');
+
 
 describe('filesystem store', () => {
   let store
@@ -22,6 +25,43 @@ describe('filesystem store', () => {
           'lemon-curd-tart',
           'rice-pudding'
         ]);
+    })
+  })
+
+  describe('get_recipe', () => {
+    let recipe
+
+    beforeEach(async () => {
+      recipe = await store.get_recipe('rice-pudding');
+    })
+
+    it('returns a Recipe', () => {
+      expect(recipe instanceof Recipe).toBeTruthy();
+    })
+
+    it('exposes the recipe title', () => {
+      expect(recipe.name).toEqual('Rice pudding');
+    })
+
+    it('exposes the attributes', () => {
+      expect(recipe.attributes).toEqual(['dessert']);
+    })
+
+    it('exposes the ingredient list', () => {
+      let ingredients = recipe.ingredients;
+      expect(ingredients.length).toEqual(6);
+      expect(ingredients[0] instanceof RecipeIngredient).toBeTruthy();
+    })
+
+    it('builds RecipeIngredient objects', () => {
+      let item = recipe.ingredients[0];
+      expect(item.ingredient).toEqual(new Ingredient('white long grain rice'));
+      expect(item.quantity).toEqual(new Quantity(100, 'g'))
+    })
+
+    it('normalises quantities', () => {
+      let item = recipe.ingredients[4];
+      expect(item.quantity).toEqual(new Quantity(1));
     })
   })
 })
