ba2df5d6f11725c0be8a5aa805d02b5c71bbba39
James Coglan <jcoglan@gmail.com>
2019-04-30 13:45:54 +0100

    Set up React build environment
    This creates the following npm scripts:
    
    - build: just runs Webpack to build the project for development
    - dist: builds the project for production
    - start: runs a webpack-watch process to continually compile
    
    The app's entry point is set to:
    
        lib/ui/app.js
    
    The bundle is saved to:
    
        web/app.bundle.js
    
    We don't do any compilation except conversion of JSX using Babel.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+
+  entry: path.resolve(__dirname, 'lib', 'ui', 'app'),
+
+  output: {
+    path: path.resolve(__dirname, 'web'),
+    filename: '[name].bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test:    /\.jsx?$/,
+        exclude: /node_modules/,
+        loader:  'babel-loader',
+        options: { presets: ['@babel/preset-react'] }
+      }
+    ]
+  }
+};
