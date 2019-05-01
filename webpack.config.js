3713eabcae9bb94108794f0cfcabe69536fe4f8b
James Coglan <jcoglan@gmail.com>
2019-05-01 11:18:53 +0100

    Enable source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  devtool: 'source-map',
 
   entry: {
     app: path.resolve(__dirname, 'lib', 'ui', 'app')
