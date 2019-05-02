329c3d30bbbeb8ae67230a51ad046d5b8577886f
James Coglan <jcoglan@gmail.com>
2019-05-02 15:57:04 +0100

    add() and update() for the Dropbox client
    The /files/upload method of the Dropbox API implements optimistic
    concurrency control: all files have a version, which Dropbox calls the
    'rev'. It returns the rev with the metadata when you download a file,
    and returns the new one when you successfully upload one. To upload a
    new version of a file, you must specify whether you believe you are
    adding a new file, or modifying an existing one. If the latter, you must
    supply the file's current rev for your upload to succeed.
    
    Here we implement two methods as a wrapper over this functionality.
    add() takes a file path and data, and update() takes these plus a rev
    value.
    
    On success, these methods return a Reponse whose meta field contains the
    new rev. On conflict errors, a special type of error is thrown so the
    caller can detect it. All other kinds of errors result in a generic
    exception.

--- spec/storage/dropbox_spec.js
+++ spec/storage/dropbox_spec.js
@@ -108,4 +108,127 @@ describe('Dropbox client', () => {
       })
     })
   })
+
+  describe('add', () => {
+    it('makes a request to the upload endpoint', async () => {
+      await this.client.add('/file.txt', 'the content');
+
+      expect(fetch).toHaveBeenCalledWith(
+        'https://content.dropboxapi.com/2/files/upload', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Authorization': 'Bearer ' + TOKEN,
+            'Content-Type': 'text/plain; charset=dropbox-cors-hack',
+            'Dropbox-API-Arg': '{"path":"/file.txt","mode":"add","autorename":false,"mute":true}'
+          },
+          body: 'the content'
+        });
+    })
+
+    describe('on successful response', () => {
+      beforeEach(() => {
+        let response = new Response(200, {
+            'Content-Type': 'application/json'
+          },
+          JSON.stringify({
+            name: 'file.txt',
+            rev: 'file-revision'
+          }));
+
+        fetch.and.returnValue(Promise.resolve(response));
+      })
+
+      it('returns the file metadata', async () => {
+        let response = await this.client.add('/file.txt', 'a');
+
+        expect(response.data).toEqual(null);
+        expect(response.meta).toEqual({ name: 'file.txt', rev: 'file-revision' });
+      })
+    })
+
+    describe('on an authorization error', () => {
+      beforeEach(() => {
+        let response = new Response(401, {
+            'WWW-Authenticate': 'Bearer realm="Dropbox-API"',
+            'Content-Type': 'application/json'
+          },
+          JSON.stringify({
+            error_summary: 'invalid_access_token/',
+            error: { '.tag': 'invalid_access_token' }
+          }));
+
+        fetch.and.returnValue(Promise.resolve(response));
+      })
+
+      it('throws an error', async () => {
+        let error;
+        try { await this.client.add('/file.txt', 'a') } catch (e) { error = e }
+
+        expect(error.type).toEqual('invalid_access_token');
+      })
+    })
+
+    describe('on a bad request', () => {
+      beforeEach(() => {
+        let response = new Response(400, { 'Content-Type': 'text/plain' }, 'Boo!');
+        fetch.and.returnValue(Promise.resolve(response));
+      })
+
+      it('throws an error', async () => {
+        let error;
+        try { await this.client.add('/file.txt', 'a') } catch (e) { error = e }
+
+        expect(error.type).toEqual('bad_request');
+        expect(error.message).toEqual('Boo!');
+      })
+    })
+
+    describe('on a rev conflict', () => {
+      beforeEach(() => {
+        let response = new Response(409, {
+            'Content-Type': 'application/json'
+          },
+          JSON.stringify({
+            error_summary: 'path/conflict/file/..',
+            error: {
+              '.tag': 'path',
+              reason: {
+                '.tag': 'conflict',
+                conflict: { '.tag': 'file' }
+              }
+            }
+          }));
+
+        fetch.and.returnValue(Promise.resolve(response));
+      })
+
+      it('throws an error', async () => {
+        let error;
+        try { await this.client.add('/file.txt', 'a') } catch (e) { error = e }
+
+        expect(error.isConflict()).toBeTruthy();
+        expect(error.type).toEqual('conflict');
+        expect(error.message).toEqual('path/conflict/file/..');
+      })
+    })
+  })
+
+  describe('update', () => {
+    it('makes a request to the upload endpoint', async () => {
+      await this.client.update('/file.txt', 'the content', 'rev-01');
+
+      expect(fetch).toHaveBeenCalledWith(
+        'https://content.dropboxapi.com/2/files/upload', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Authorization': 'Bearer ' + TOKEN,
+            'Content-Type': 'text/plain; charset=dropbox-cors-hack',
+            'Dropbox-API-Arg': '{"path":"/file.txt","mode":{".tag":"update","update":"rev-01"},"autorename":false,"mute":true}'
+          },
+          body: 'the content'
+        });
+    })
+  })
 })
